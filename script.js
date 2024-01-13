a451e9859165f106f7316ec3ea627d1811a1b2f3
Penelope Larkin <125135975+penelopelarkin@users.noreply.github.com>
2024-01-13 17:52:42 -0500

    Add files via upload
    Committing from replit

---
+++ script.js
@@ -0,0 +1,17 @@
+function showUnhealthy() {
+  var x = document.getElementById("Unhealthy Plant");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function showHealthy() {
+  var x = document.getElementById("Healthy Plant");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
